4206eb74854705cf2e06f20d59a3d9bd0df990ef
Leonardo Gonçalves <leonardo.goncalves@totvs.com.br>
2018-06-27 13:12:29 -0300

    pcontagem de movimentos

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
-/*
- * Create a list that holds all of your cards
- */
+
 var cards = [
     "card1",
     "card2",
@@ -44,129 +42,120 @@ var cards2 = [{
 
 }]
 
- // Display the cards on the page
- //   - shuffle the list of cards using the provided "shuffle" method below
     shuffle(cards);
     shuffle(classes);
- //   - loop through each card and create its HTML
- //   - add each card's HTML to the page
-    for (i = 0 ; i < cards.length ; i++){
 
-        //create element li
-        let li = document.createElement('li');
-        let t = document.createTextNode("");
-        li.appendChild(t);
-        document.getElementsByClassName("deck")[0].appendChild(li);
-        li.setAttribute('class', 'card');
-        //li.setAttribute('id', 'card');        
-        li.addEventListener('click', turnCard.bind(null, i));
+for (i = 0 ; i < cards.length ; i++){
 
-        // create element i
-        var ei = document.createElement('i');
-        var et = document.createTextNode("");
-        ei.append(et);
-        document.getElementsByClassName("card")[i].appendChild(ei);
-        ei.setAttribute('class', classes[i]);
-        ei.setAttribute('id', classes[i]);
+    //create element li
+    let li = document.createElement('li');
+    let t = document.createTextNode("");
+    li.appendChild(t);
+    document.getElementsByClassName("deck")[0].appendChild(li);
+    li.setAttribute('class', 'card');       
+    li.addEventListener('click', turnCard.bind(null, i));
+
+    // create element i
+    var ei = document.createElement('i');
+    var et = document.createTextNode("");
+    ei.append(et);
+    document.getElementsByClassName("card")[i].appendChild(ei);
+    ei.setAttribute('class', classes[i]);
+    ei.setAttribute('id', classes[i]);
+}
+
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
     }
+    return array;
+}
 
-// Shuffle function from http://stackoverflow.com/a/2450976
-        function shuffle(array) {
-            var currentIndex = array.length, temporaryValue, randomIndex;
+var card1 = {
+    classe: '',
+    index: ''
+};
 
-            while (currentIndex !== 0) {
-                randomIndex = Math.floor(Math.random() * currentIndex);
-                currentIndex -= 1;
-                temporaryValue = array[currentIndex];
-                array[currentIndex] = array[randomIndex];
-                array[randomIndex] = temporaryValue;
-            }
-            return array;
-        }
+var card2 = {
+    classe: '',
+    index: ''
+};
 
-/*
- * Configure o ouvinte de eventos para um cartão. Se um cartão for clicado:
- * - exibe o símbolo do cartão (coloque esta funcionalidade em outra função que você chama deste)
- * - adicione o cartão a uma * lista * de cartões "abertos" (coloque esta funcionalidade em outra função que você chama desta)
- * - se a lista já tiver outro cartão, verifique se os dois cartões correspondem
- * + se as cartas coincidirem, bloqueie as cartas na posição aberta (coloque esta funcionalidade em outra função que você chama desta)
- * + se os cartões não coincidirem, remova os cartões da lista e oculte o símbolo do cartão (coloque esta funcionalidade em outra função
-     que você chama deste)
- * + incrementa o contador de movimentos e exibe-o na página (coloca esta funcionalidade em outra função que você chama desta)
- * + se todos os cartões tiverem correspondido, exibir uma mensagem com a pontuação final (colocar essa funcionalidade em outra função que
-     você chama a partir desta)
- */
+var cardClass = '';
 
-    var card1 = {
+var countMatch = '';
+
+var countMoves = '';
+
+function resetCard() {
+    document.getElementsByClassName("card")[card1.index].setAttribute('class', 'card');
+    document.getElementsByClassName("card")[card2.index].setAttribute('class', 'card');
+
+    card1 = {
+        classe: '',
+        index: ''
+    };        
+    card2 = {
         classe: '',
         index: ''
     };
 
-    var card2 = {
-        classe: '',
-        index: ''
-    };
+    cardClass = '';
 
-    var cardClass = '';
+    countMoves++;
 
-    function resetCard() {
-        document.getElementsByClassName("card")[card1.index].setAttribute('class', 'card');
-        document.getElementsByClassName("card")[card2.index].setAttribute('class', 'card');
+    document.getElementsByClassName("moves")[0].innerHTML = countMoves;
+}
 
-        card1 = {
-            classe: '',
-            index: ''
-        };        
-        card2 = {
-            classe: '',
-            index: ''
-        };
+function turnCard(i){
+    var cardIn = document.getElementsByClassName("card")[i];
+    cardIn.setAttribute('class', 'card open show');
 
-        cardClass = '';
-    }
+    cardClass = document.getElementById(classes[i]).className;
 
-    function turnCard(i){
-        var cardIn = document.getElementsByClassName("card")[i];
-        cardIn.setAttribute('class', 'card open show');
+    if (card1.classe !== '')
+    {
+        card2.classe = cardClass;
+        card2.index = i;        
 
-        console.log(cardIn);
-
-        cardClass = document.getElementById(classes[i]).className;
-
-        if (card1.classe !== '')
+        if (card1.classe === card2.classe)
         {
-            card2.classe = cardClass;
-            card2.index = i;
+            card1 = {
+                classe: '',
+                index: ''
+            };                
+            card2 = {
+                classe: '',
+                index: ''
+            };
+            cardClass = '';
 
-            console.log("card 2- if  - " + card2.index);
+            countMatch++;
 
+            countMoves++
 
-            if (card1.classe === card2.classe)
+            document.getElementsByClassName("moves")[0].innerHTML = countMoves;
+
+            if (countMatch == 8)
             {
-                card1 = {
-                    classe: '',
-                    index: ''
-                };                
-                card2 = {
-                    classe: '',
-                    index: ''
-                };
-                cardClass = '';
-            }
-            else
-            {                
-
-                console.log(i);
-                console.log(card1.index);
-
-                setTimeout(resetCard, 500);
-            }         
+                alert("Fim de jogo");
+            } 
         }
         else
-        {
-            card1.classe = cardClass;
-            card1.index = i;
-
-            console.log("card 1- else - " + card1.index);
-        }          
+        {                
+            setTimeout(resetCard, 500);
+        }         
     }
+    else
+    {
+        card1.classe = cardClass;
+        card1.index = i;
+    }   
+}
